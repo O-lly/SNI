a99ab363dda4c83c052f533385c16d51b6eda979
O-lly <olintofaco@alu.ufc.br>
2025-05-18 21:14:36 -0300

    main

--- src/main.py
+++ src/main.py
@@ -13,8 +13,60 @@ from services.publishers.twitter_publisher import TwitterPublisher
 from services.publishers.email_publisher import EmailPublisher
 
 # Credenciais
-reddit_credentials = json.load(open('credentials/reddit_credentials.json'))
-twitter_credentials = json.load(open('credentials/twitter_credentials.json'))
-telegram_credentials = json.load(open('credentials/telegram_credentials.json'))
-email_credentials = json.load(open('credentials/email_credentials.json'))
+reddit_credentials = json.load(open('../credentials/reddit_credentials.json'))
+twitter_credentials = json.load(open('../credentials/twitter_credentials.json'))
+telegram_credentials = json.load(open('../credentials/telegram_credentials.json'))
+email_credentials = json.load(open('../credentials/email_credentials.json'))
 
+
+def main():
+
+    # 1) Inicializa DB e histórico
+    init_db()
+    repo = PostHistoryRepository()
+
+    # 2) Carrega config
+    config = json.load(open('../config/config.json'))
+
+    # 3) Instancia fetchers
+    fetchers = [
+        RedditFetcher(
+            credentials=reddit_credentials,
+            repository=repo,
+            subreddit='HonkaiStarRail',
+            filter=config['reddit_filter'],
+            limit=config['reddit_limit']
+        ),
+    ]
+
+    # 4) Instancia formatters
+    formatters = {
+        'reddit': RedditFormatter(),
+    }
+
+    # 5) Instancia publishers
+    publishers = []
+
+    if config['twitter_send']:
+        from tweepy import TwitterClient
+        twitter_client = TwitterClient(twitter_credentials)
+        publishers.append(TwitterPublisher(twitter_client, repo))
+    if config['email_send']:
+        import mail
+        publishers.append(EmailPublisher(
+            mailer=mail,
+            repository=repo,
+            receiver=email_credentials['receiver']
+        ))
+
+    # 6) Cria e roda orquestrador
+    orchestrator = BotOrchestrator(
+        fetchers=fetchers,
+        formatters=formatters,
+        publishers=publishers,
+        interval=600
+    )
+    asyncio.run(orchestrator.run())
+
+if __name__ == "__main__":
+    main()
